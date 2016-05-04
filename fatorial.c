0ab5b02ccb67025e6b0e238b70490817c7d690ed
JOHANNES PORTZ DIAS <16104044@l1801174.portoalegre.pucrsnet.br>
2016-05-04 11:03:04 -0300

    "fatorial 04/05/2016"

--- fatorial.c
+++ fatorial.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
 
+double fatorial(double);
+
+double fatorial(double n)
+{
+	double r=1L;
+	double c=n;
+	while(c>0L)
+	{
+		r=r*c;
+		c=c-1L;
+	}
+	
+	return r;
+}
+int main ()
+{
+	double n;
+	printf("Insira o número: ");
+	scanf("%lf", &n);
+	double f=fatorial(n);
+	printf("Número=%.0lf, Fatorial=%.0lf\n", n, f);
+	return 0;
+}
